cab4087c0784337d34f5bc1d6a5c5b679f0f092b
Kerim Satirli <k.satirli@gmail.com>
2022-09-21 17:55:47 +0200

    bumps HCP Provider version from `0.43.0` to `0.44.0`

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.43.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.44.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.43.0, < 1.0.0"
+      version = ">= 0.44.0, < 1.0.0"
     }
   }
 
