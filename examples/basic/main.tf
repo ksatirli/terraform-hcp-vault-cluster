ecf337106f8b6607bfdd28d37e6d47b8416e6a66
Kerim Satirli <k.satirli@gmail.com>
2022-09-27 19:06:02 +0200

    bumps HCP Provider version from `0.44.0` to `0.45.0`

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,15 +1,21 @@
 # retrieve HVN ID of a previously created HVN
 # see https://registry.terraform.io/providers/hashicorp/hcp/latest/docs/data-sources/hvn
 data "hcp_hvn" "main" {
-  hvn_id = "example"
+  hvn_id = "workloads-aws-us-west-2"
 }
 
 module "hcp_vault" {
   source = "../.."
 
-  cluster_id        = "module-test"
+  cluster_id        = "simple"
   hvn_id            = data.hcp_hvn.main.hvn_id
   min_vault_version = "1.10.1"
   public_endpoint   = true
   tier              = "dev"
+
+  metrics_config = {
+    enabled            = true
+    datadog_api_key    = "5b7bc5ad87453e010d6dade19c14aeb5"
+    datadog_region     = "us1"
+  }
 }
