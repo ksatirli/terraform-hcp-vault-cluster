cb3311e7bb2d7c431bca841ea22656a6bd48331c
Kerim Satirli <k.satirli@gmail.com>
2022-10-05 12:00:41 +0200

    removes redundant data lookups

--- examples/audit-log-config-datadog/main.tf
+++ examples/audit-log-config-datadog/main.tf
@@ -1,14 +1,8 @@
-# retrieve HVN ID of a previously created HVN
-# see https://registry.terraform.io/providers/hashicorp/hcp/latest/docs/data-sources/hvn
-data "hcp_hvn" "main" {
-  hvn_id = var.hcp_hvn_id
-}
-
 module "hcp_vault" {
   source = "../.."
 
   cluster_id        = "audit-log-config-datadog-example"
-  hvn_id            = data.hcp_hvn.main.hvn_id
+  hvn_id            = var.hcp_hvn_id
   min_vault_version = "1.10.3"
   public_endpoint   = true
   tier              = "starter_small"
