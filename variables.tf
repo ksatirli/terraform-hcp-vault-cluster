90fd74cf16b09d847400b4e9fd2571bb2b64567f
Kerim Satirli <k.satirli@gmail.com>
2023-01-23 15:44:48 +0100

    temporarily remove `major_version_upgrade_config`

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "major_version_upgrade_config" {
   description = "The Major Version Upgrade configuration. Only applied on Clusters of tier `standard_`, or `plus_`."
 #
 #  default = {
-#    upgrade_type            = "SCHEDULED"
+#    upgrade_type            = "AUTOMATIC"
 #    maintenance_window_day  = "TUESDAY"
 #    maintenance_window_time = "WINDOW_12PM_4PM"
 #  }
