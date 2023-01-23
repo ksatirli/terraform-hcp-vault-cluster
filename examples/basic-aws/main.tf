7a0e37f79226e940cea6390d3a5332756b3d10f9
Kerim Satirli <k.satirli@gmail.com>
2023-01-23 15:13:30 +0100

    bumps HCP Vault version from `1.11.3` to `1.12.2`

--- examples/basic-aws/main.tf
+++ examples/basic-aws/main.tf
@@ -3,7 +3,7 @@ module "hcp_vault" {
 
   cluster_id        = "simple-example"
   hvn_id            = var.hcp_hvn_id
-  min_vault_version = "1.11.3"
+  min_vault_version = "1.12.2"
   public_endpoint   = true
   tier              = "starter_small"
 }
