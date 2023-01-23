bbde2161a6b7cc373aa49059b818da9a870aed07
Kerim Satirli <k.satirli@gmail.com>
2023-01-23 15:09:59 +0100

    bumps HCP Provider from `0.45.0` to `0.53.0`

--- examples/basic-azure/terraform.tf
+++ examples/basic-azure/terraform.tf
@@ -7,10 +7,10 @@ terraform {
       version = ">= 3.25.0, < 4.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.45.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.53.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.45.0, < 1.0.0"
+      version = ">= 0.53.0, < 1.0.0"
     }
   }
 
--- examples/metrics-config-datadog/terraform.tf
+++ examples/metrics-config-datadog/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.45.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.53.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.45.0, < 1.0.0"
+      version = ">= 0.53.0, < 1.0.0"
     }
   }
 
