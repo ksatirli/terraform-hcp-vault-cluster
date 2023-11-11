748e818740bed8a2e498553145a901140f23e0a2
Kerim Satirli <k.satirli@gmail.com>
2023-11-11 12:15:35 -0800

    moves minimal HCP Vault version to `1.14.3`

--- examples/basic-aws/main.tf
+++ examples/basic-aws/main.tf
@@ -3,7 +3,7 @@ module "hcp_vault" {
 
   cluster_id        = "simple-example"
   hvn_id            = var.hcp_hvn_id
-  min_vault_version = "1.14.1"
+  min_vault_version = "1.14.3"
   public_endpoint   = true
   tier              = "starter_small"
 }
