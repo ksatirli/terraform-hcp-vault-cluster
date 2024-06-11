79af394a5f63cba715497960201f13a72b8904a1
Kerim Satirli <k.satirli@gmail.com>
2024-06-11 23:13:34 +0200

    updates Terraform providers

--- examples/basic-azure/terraform.tf
+++ examples/basic-azure/terraform.tf
@@ -1,22 +1,22 @@
 terraform {
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/azuread/2.45.0
+    # see https://registry.terraform.io/providers/hashicorp/azuread/2.51.0
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.45.0, < 3.0.0"
+      version = ">= 2.51.0, < 3.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/azurerm/3.80.0
+    # see https://registry.terraform.io/providers/hashicorp/azurerm/3.107.0
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.80.0, < 4.0.0"
+      version = ">= 3.107.0, < 4.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.78.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.91.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.78.0, < 1.0.0"
+      version = ">= 0.91.0, < 1.0.0"
     }
   }
 
